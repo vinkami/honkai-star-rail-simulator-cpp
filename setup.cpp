55dfce2d0efddfdea124e729b25aebde42acdff2
vinkami <vinkami.krip@gmail.com>
2024-04-11 01:50:22 +0800

    Created and declared functions

---
+++ setup.cpp
@@ -0,0 +1,21 @@
+//
+// Created by vinkami on 11/4/2024.
+//
+
+#include "setup.h"
+#include "state.h"
+#include <iostream>
+using namespace std;
+
+void teamMenu(State &state) {
+    cout << "Team Menu" << endl;
+}
+
+void battleMenu(State &state) {
+    cout << "Battle Menu" << endl;
+}
+
+void settingsMenu(State &state) {
+    cout << "Settings Menu" << endl;
+}
+
---
+++ setup.h
@@ -0,0 +1,16 @@
+//
+// Created by vinkami on 11/4/2024.
+//
+
+#ifndef PROJECT_SETUP_H
+#define PROJECT_SETUP_H
+
+#include "state.h"
+
+void teamMenu(State &state);
+
+void battleMenu(State &state);
+
+void settingsMenu(State &state);
+
+#endif //PROJECT_SETUP_H
