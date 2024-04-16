fdf84c44dc1eb65b7f56c5c795fda779168de806
vinkami <vinkami.krip@gmail.com>
2024-04-17 05:03:41 +0800

    Moved State to start of main() Added team, battle and settings commands

--- main.cpp
+++ main.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 #include "battle.h"
 #include "character.h"
 #include "state.h"
 #include "setup.h"
+#include "function.h"
 
 using namespace std;
 
 int main() {
-    cout << "Welcome to Honkai: Star Rail battle simulator.\nType 'help' for a list of commands." << endl;
-    string cmd;
-    while (cin >> cmd) {
+    vector<Character> allies {};
+    vector<Character> enemies {};
+    Character round("Round", 100, 0, 0, 0, 0, 0);
+    State state(allies, enemies, round);
+
+    cout << "Welcome to Honkai: Star Rail battle simulator.\nType 'help' for a list of commands.\nEnter command:";
+    string input;
+    while (getline(cin, input)) {
+        stringstream ss(input);
+        string cmd;
+        ss >> cmd;
         if (cmd == "exit") {
             break;
         } else if (cmd == "help") {
-            cout << "Commands: help, exit" << endl;
+            printHelp("main");
         } else if (cmd == "start") {
-            vector<Character> allies = teamMenu();
-            vector<Character> enemies {Character("E1", 100), Character("E2", 200), Character("E3", 300), Character("E4", 400), Character("E5", 500)};
-            State state(allies, enemies);
-
             battleStart(state);
-            while(gameLoop(state));
+            while (gameLoop(state));
             battleEnd(state);
 
             cout << "Battle ended." << endl;
+        } else if (cmd == "team") {
+            teamMenu(state);
+        } else if (cmd == "battle") {
+            battleMenu(state);
+        } else if (cmd == "settings") {
+            settingsMenu(state);
         } else {
-            cout << "Unknown command: " << cmd << endl;
+            cout << "Unknown command" << endl;
         }
+
+        cout << "Enter command:";
     }
     cout << "Goodbye!" << endl;
 }
