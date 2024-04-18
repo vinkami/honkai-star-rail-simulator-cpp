1f83205ce0f61afb1d89aa2294feae65f505e292
vinkami <vinkami.krip@gmail.com>
2024-04-19 04:32:19 +0800

    Added dummy functions to properly initiate the functions

--- character.h
+++ character.h
@@ -8,14 +8,17 @@
 using namespace std;
 
 class Character{
+private:
+    function<void(Character &self, State &state)> emptyAbility = [](Character &self, State &state) {};
+    function<void(State &state)> emptyRoundAction = [](State &state) {};
 public:
     string name, faction="ally";  // faction: ally / enemy / round
     double speed, hp, atk, def;
     double critRate, critDamage;
     double resetRemTime, remTime;
 
-    function<void(State &state)> basicAtk{}, skill{}, ult{};
-    function<void(State &state)> startBattle{}, startRound{}, endRound{};
+    function<void(Character &self, State &state)> basicAtk = emptyAbility, skill = emptyAbility, ult = emptyAbility;
+    function<void(State &state)> startBattle = emptyRoundAction, startRound = emptyRoundAction, endRound = emptyRoundAction;
 
 
     Character(string name, double speed, double hp, double atk, double def, double critRate, double critDamage);
