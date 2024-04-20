206d8aead72619e9336e205bd425fba6015cf4be
vinkami <vinkami.krip@gmail.com>
2024-04-21 01:25:40 +0800

    Added functions for all 4 types of attack patterns Removed low-level attack() and getNonCritDamage()

--- logic.h
+++ logic.h
@@ -8,7 +8,11 @@
 #include "character.h"
 
 bool hit(double chance);
-double getNonCritDamage(double atk, double skillMultiplier, double def, double defIgnore=0);
-void attack(Character &attacker, Character &defender, double skillMultiplier);
+//double getNonCritDamage(double atk, double skillMultiplier, double def, double defIgnore=0);
+//void attack(Character &attacker, Character &defender, double skillMultiplier);
 
+void singleAttack(State &state, Character &attacker, int target, double skillMultiplier);
+void blastAttack(State &state, Character &attacker, int target, double mainSkillMultiplier, double adjacentSkillMultiplier);
+void aoeAttack(State &state, Character &attacker, double skillMultiplier);
+void bounceAttack(State &state, Character &attacker, int target, double skillMultiplier, int bounceCount);
 #endif //PROJECT_LOGIC_H
