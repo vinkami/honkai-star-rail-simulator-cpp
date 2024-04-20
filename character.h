0b268747450085531a303530c210d02e4e997e53
vinkami <vinkami.krip@gmail.com>
2024-04-21 03:54:23 +0800

    Added warnings Reformatted code Added default functions for onHit(), startRound() and endRound()

--- character.h
+++ character.h
@@ -11,7 +11,6 @@ using namespace std;
 class Character{
 private:
     function<void(Character &self, State &state)> emptyAbility = [](Character &self, State &state) {state.timelineProceed = true;};
-    function<void(State &state)> emptyRoundAction = [](State &state) {};
 public:
     string name, faction="ally";  // faction: ally / enemy / round
     double baseSpeed, baseHp, baseAtk, baseDef, baseCritRate, baseCritDamage, maxEnergy;
@@ -19,8 +18,14 @@ public:
     double resetRemTime, remTime;
 
     vector<Effect> effects{};
-    function<void(Character &self, State &state)> basicAtk = emptyAbility, skill = emptyAbility, ult = emptyAbility;
-    function<void(State &state)> startBattle = emptyRoundAction, startRound = emptyRoundAction, endRound = emptyRoundAction;
+    function<void(Character &self, State &state)>
+        basicAtk = emptyAbility,
+        skill = emptyAbility,
+        ult = emptyAbility,
+        onHit = [](Character &self, State &state) {self.energy += 5;},
+        startBattle = emptyAbility,
+        startRound = [](Character &self, State &state) {for (auto &efx: self.effects) efx.startRound(state);},
+        endRound = [](Character &self, State &state) {for (auto &efx: self.effects) efx.endRound(state);};
 
 
     Character(string name, double speed, double hp, double atk, double def, double critRate, double critDamage, double maxEnergy);
@@ -29,7 +34,7 @@ public:
     void reset();
     void print() const;
     int getEffectLoc(const string &efxName);
-    Effect &getEffect(const string &efxName);
+    Effect &getEffect(const string &efxName);  // NOTE: DO NOT USE WHEN YOU ARE NOT SURE THAT THE EFFECT EXISTS
 };
 
 #endif //PROJECT_CHARACTER_H
