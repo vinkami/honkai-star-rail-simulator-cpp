298ffacd57a1637b5a1585b4a2277465b18784e6
vinkami <vinkami.krip@gmail.com>
2024-04-21 01:24:44 +0800

    Converted Jingliu's syzygy and spectral transmigration to effects Fixed typo Added name of abilities as comments Fixed skill point not decreasing in skills Fixed enemies not having "enemy" faction tag Added maxEnergy when getting playable characters

--- function.cpp
+++ function.cpp
@@ -50,7 +50,7 @@ int selectTarget(vector<Character>& characters) {
         for (int i = 0; i < characters.size(); ++i) {
             cout << i + 1 << ". " << characters[i].name << " ";
         }
-        cout << endl << "Select a move (1-" << characters.size() << " or {help|exit}, anything else defaults to 1): ";
+        cout << endl << "Select a target (1-" << characters.size() << " or {help|exit}, anything else defaults to 1): ";
         string line, move;
         getline(cin, line);
         stringstream ss(line);
@@ -89,69 +89,61 @@ int selectTarget(vector<Character>& characters) {
 // If u can't 100% copy the ability, just make a similar one and write a comment to explain what changes u made
 void insertCharacterAbility(Character &character) {
     if (character.name == "Clara") {
-        character.basicAtk = [](Character &self, State &state) {
+        character.basicAtk = [](Character &self, State &state) {  // I Want to Help
             int target = selectTarget(state.enemies);
             Character &enemy = state.enemies[target];
             if (hit(50)) slowPrint("クラーラ：気を付けて、スヴァローグ！\n", {37});
             else slowPrint("スヴァローグ：排除する。\n", {36});
-            attack(self, enemy, 1.0);
+            singleAttack(state, self, target, 1.0);
         };
-        character.skill = [](Character &self, State &state) {
+        character.skill = [](Character &self, State &state) {  // Svarog Watches Over You
+            state.skillPoint--;
             if (hit(50)) slowPrint("スヴァローグ：隠れろ。\n", {36});
             else slowPrint("スヴァローグ：殲滅開始。\n", {36});
-            for (auto &enemy : state.enemies) {
-                attack(self, enemy, 1.2);
-            }
+            aoeAttack(state, self, 1.2);
         };
-        character.ult = [](Character &self, State &state) {
+        character.ult = [](Character &self, State &state) {  // Promise, Not Command
             slowPrint("クラーラ：クラーラも…みんなを守りたい！       \nクラーラ：助けて、スヴァローグ！\n", {37});
             // Todo: Implement ultimate ability
         };
     }
     else if (character.name== "Jingliu") {
-        character.basicAtk = [](Character &self, State &state) {
+        Effect syzygy("Syzygy", -1, 0);
+        Effect transmigration("Spectral Transmigration", -1, 0);
+        character.effects.push_back(syzygy);
+        character.effects.push_back(transmigration);
+
+        character.basicAtk = [](Character &self, State &state) {  // Lucent Moonglow
             int target = selectTarget(state.enemies);
-            Character &enemy = state.enemies[target];
-            slowPrint("鏡流 : 切先は戻らぬ！\n",{34});
-            attack(self,enemy,1.0);
+            slowPrint("鏡流：切先は戻らぬ！\n", {34});
+            singleAttack(state, self, target, 1.0);
         };
-        character.skill = [](Character &self, State &state){
-            if (self.stack < 2) {
+        character.skill = [&syzygy, &transmigration](Character &self, State &state){
+            if (transmigration.stack == 0) {  // Transcendent Flash
+                state.skillPoint--;
                 int target = selectTarget(state.enemies);
-                Character &enemy= state.enemies[target];
-                self.critRate=45;
-                slowPrint("鏡流 : 飛光よ、差せ！\n", {34});
-                self.stack += 1;
-                slowPrint("Syzygy stack: ",{34});
-                string stack= to_string(self.stack);
-                slowPrint(stack,{34});
-                cout << endl;
-                attack(self,enemy,2.0);
-            } if (self.stack >=2){
-                    self.critRate=95;
-                    if (self.stack==2){
-                    self.stack=5+2;   //use 5 stack to present her in talent mode, + the stack remains
-                    slowPrint("鏡流 : 乗月返真。\n",{34});
-                    } int target = selectTarget(state.enemies);
-                    Character &enemy= state.enemies[target];
-                    slowPrint("鏡流 : 月光を剣とせん。\n",{34});
-                    attack(self,enemy,2.5);
-                    if (target>0){
-                        Character &enemy= state.enemies[target-1];
-                        attack(self,enemy,1.25);
-                    } if (target-1<state.enemies.size()){
-                        Character &enemy= state.enemies[target+1];
-                        attack(self,enemy,1.25);
-                    }
-                    self.stack-=1;
-                    slowPrint("Syzygy stack: ",{34});
-                    string stack= to_string(self.stack-5);
-                    slowPrint(stack,{34});
-                    cout << endl;
-                    if (self.stack==5)
-                        self.stack=0;
+                slowPrint("鏡流：飛光よ、差せ！\n", {34});
+                syzygy.stack += 1;
+                slowPrint("Syzygy stack: " + to_string(syzygy.stack) + "\n",{34});
+                singleAttack(state, self, target, 2.0);
+
+                if (syzygy.stack >= 2) {
+                    transmigration.stack = 1;
+                    self.critRate += 50;
+                    slowPrint("鏡流：乗月返真。\n",{34});
                 }
-            };
+            } else {  // Moon On Glacial River
+                int target = selectTarget(state.enemies);
+                slowPrint("鏡流：月光を剣とせん。\n",{34});
+                blastAttack(state, self, target, 2.5, 1.25);
+                syzygy.stack -= 1;
+                if (syzygy.stack == 0) {
+                    transmigration.stack = 0;
+                    self.critRate -= 50;
+                }
+                slowPrint("Syzygy stack: " + to_string(syzygy.stack) + " (Spectral Transmigration mode active)\n",{34});
+            }
+        };
     }
 }
     // Todo: Add more characters
@@ -194,7 +186,8 @@ vector<Situation> getSituations() {
         ss >> atk >> comma;
         ss >> def;
 
-        Character temp(name, speed, hp, atk, def, 0, 0);
+        Character temp(name, speed, hp, atk, def, 0, 0, 0);
+        temp.faction = "enemy";
         situations[situationNo].enemies.push_back(temp);
     }
     enemyFile.close();
@@ -209,7 +202,7 @@ vector<Character> getPlayableCharacters() {
     while (getline(characterFile, line)) {  // comma separated values
         stringstream ss(line);
         string name;
-        double speed, hp, atk, def, critRate, critDamage;
+        double speed, hp, atk, def, critRate, critDamage, maxEnergy;
         char comma;
 
         getline(ss, name, ',');
@@ -218,10 +211,10 @@ vector<Character> getPlayableCharacters() {
         ss >> atk >> comma;
         ss >> def >> comma;
         ss >> critRate >> comma;
-        ss >> critDamage;
+        ss >> critDamage >> comma;
+        ss >> maxEnergy;
 
-        Character temp(name, speed, hp, atk, def, critRate, critDamage);
-        temp.stack=0;
+        Character temp(name, speed, hp, atk, def, critRate, critDamage, maxEnergy);
         insertCharacterAbility(temp);
         playableCharacters.push_back(temp);
     }
